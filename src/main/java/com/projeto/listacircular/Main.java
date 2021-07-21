d4a2eabda1092b7db3b844ccd8972e32c6c1b5ee
Jose Pereira <josepsneto@gmail.com>
2021-07-21 18:23:29 -0300

    Implementa a Lista Circular

--- src/main/java/com/projeto/listacircular/Main.java
+++ src/main/java/com/projeto/listacircular/Main.java
@@ -1,4 +1,25 @@
 package com.projeto.listacircular;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ListaCircular<String> minhaLista = new ListaCircular<>();
+
+        minhaLista.add("C0");
+        System.out.println(minhaLista);
+
+        minhaLista.remove(0);
+        System.out.println(minhaLista);
+
+        minhaLista.add("C1");
+        System.out.println(minhaLista);
+        minhaLista.add("C2");
+        minhaLista.add("C3");
+        System.out.println(minhaLista);
+
+        System.out.println(minhaLista.get(0));
+        System.out.println(minhaLista.get(1));
+
+    }
 }
